f1eec12343206bcd4dbc3f277c6f6d7f6104eb6a
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-13 14:40:57 +0100

    docs(main): add docstrings for main method

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from src.manager import Manager
 
 
 def main():
+    """Entry point of application
+        Initialize main class and start main loop
+    """
     manager = Manager()
     manager.run()
 
