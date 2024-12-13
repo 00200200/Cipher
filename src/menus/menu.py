5d8b7d3888285eb0c8477fd5f934ca6490bb3ffc
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-13 09:25:33 +0100

    docs(menu): add docstrings to menu class and show_menu method.

--- src/menus/menu.py
+++ src/menus/menu.py
@@ -1,6 +1,15 @@
 class Menu:
+    """
+    Represents the menu for Cipher Application
+
+    Class provides a static method to display the main menu
+    with app options
+    """
     @staticmethod
     def show_menu():
+        """
+        Displays the main menu for Cipher Application.
+        """
         print("-" * 24)
         print("|", f"CIPHER MENU".center(20), "|")
         print("-" * 24)
