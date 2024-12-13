97fbfc85d116cb831db458981d2de2472bc171dd
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-13 17:40:14 +0100

    docs(buffer): add docstrings to  buffer

--- src/data/buffer.py
+++ src/data/buffer.py
@@ -2,12 +2,23 @@ from src.data.text import Text
 
 
 class Buffer:
+    """Stores and manage data during program execution. """
+
     def __init__(self):
         self.data: list[Text] = []
 
     def add(self, text: Text):
+        """
+        Adds a new Text object to buffer list
+
+        :param text: Text object to add
+        :return: None
+        """
         self.data.append(text)
 
     def get_all(self):
+        """
+        Returns list of text objects in a buffer list.
+        :return: list of Text objects
+        """
         return self.data
-
