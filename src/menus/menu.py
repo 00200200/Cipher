a684bfd83ec814f64d14881298b68b4427850c20
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-13 09:22:16 +0100

    feat (menu) : add basic menu structure for user interface.

---
+++ src/menus/menu.py
@@ -0,0 +1,15 @@
+class Menu:
+    @staticmethod
+    def show_menu():
+        print("-" * 24)
+        print("|", f"CIPHER MENU".center(20), "|")
+        print("-" * 24)
+        print("|   1. Encrypt Text.   |")
+        print("|   2. Decrypt Text.   |")
+        print("|   4. Show buffer     |")
+        print("|   5. Load from File. |")
+        print("|   6. Save to File.   |")
+        print("|   7. Exit            |")
+        print("-" * 24)
+
+
