c09e7496e0bfff570e1a2eee31fec6e100b7b27a
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-13 14:44:07 +0100

    docs(data): add docstring for text dataclass

--- src/data/text.py
+++ src/data/text.py
@@ -3,6 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Text:
+    """
+    Represents a text object
+
+    text: content
+    rot_type: type of encryption (rot13 , rot47)
+    status: status : (encyrpted,decrypted)
+    """
     text: str
     rot_type: str
     status: str
