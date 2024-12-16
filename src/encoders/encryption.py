dd9aa2a7a9be09457a956f5d3cef036c71f77036
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 13:52:31 +0100

    docs(encryption): add costrings to encryption class and methods.

--- src/encoders/encryption.py
+++ src/encoders/encryption.py
@@ -2,12 +2,24 @@ from abc import abstractmethod, ABC
 
 
 class Encryption(ABC):
+    """
+    Base abstract class for encryption strategies
+    """
 
     @abstractmethod
     def encrypt(self, text: str) -> str:
-
+        """
+        Encrypt the given text.
+        :param text: Text to encrypt
+        :return: Encrypted text
+        """
         pass
 
     @abstractmethod
     def decrypt(self, text: str) -> str:
+        """
+        Decrypt the given text.
+        :param text:  Text to decrypt.
+        :return: Decrypted text.
+        """
         pass
