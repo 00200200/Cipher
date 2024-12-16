8ea5f5f00ca8f5a1263137e4e8c0b23ecbb063ca
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 13:51:03 +0100

    refactor(encryption): add type annotation to encrypt and decrypt methods.

--- src/encoders/encryption.py
+++ src/encoders/encryption.py
@@ -2,10 +2,12 @@ from abc import abstractmethod, ABC
 
 
 class Encryption(ABC):
+
     @abstractmethod
-    def encrypt(self):
+    def encrypt(self, text: str) -> str:
+
         pass
 
     @abstractmethod
-    def decrypt(self):
+    def decrypt(self, text: str) -> str:
         pass
