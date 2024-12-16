763fef4956705dddad5b9de9f03d37f468a832f7
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 19:13:56 +0100

    docs(test_text): add docstrings to text testing method.

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,7 +1,12 @@
 from src.data.text import Text
 
 
-def test_text():
+def test_text() -> None:
+    """
+    Tests the initialization of Text dataclass
+    Ensures that attributes are correctly assigned.
+    :return:None
+    """
     text = Text("hello", "rot13", "encrypted")
     assert text.text == "hello"
     assert text.rot_type == "rot13"
