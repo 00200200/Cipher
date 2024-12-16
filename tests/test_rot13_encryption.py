4cfc6ee3b274dd00ed2778fcd8a86ad8aee35a93
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 19:12:00 +0100

    docs(test_rot13_encryption): add docstrings to Rot13Encryption testing methods.

--- tests/test_rot13_encryption.py
+++ tests/test_rot13_encryption.py
@@ -6,7 +6,11 @@ import pytest
     ("hello", "uryyb"), ("HELLO", "URYYB"), ("12345", "12345"), ("''", "''"),
     ("test example", "grfg rknzcyr"), ("TEST EXAMPLE", "GRFG RKNZCYR"), ("Test Example", "Grfg Rknzcyr"),
 ])
-def test_rot13_encrypt(input_text, output):
+def test_rot13_encrypt(input_text, output) -> None:
+    """
+    Tests the encryption() method of Rot13Encryption class
+    Ensures that encryption workds correctly
+    """
     rot13 = Rot13Encryption()
     assert rot13.encrypt(input_text) == output
 
@@ -16,5 +20,9 @@ def test_rot13_encrypt(input_text, output):
     ("grfg rknzcyr", "test example"), ("GRFG RKNZCYR", "TEST EXAMPLE"), ("Grfg Rknzcyr", "Test Example"),
 ])
 def test_rot13_decrypt(input_text, output):
+    """
+    Tests the decrypt() method of Rot13Encryption class
+    Ensures that decryption workds correctly
+    """
     rot13 = Rot13Encryption()
     assert rot13.decrypt(input_text) == output
