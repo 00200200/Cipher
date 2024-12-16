771c1f16e815bad60cc42f5b4688f5f38e5fa247
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 14:13:18 +0100

    feat(rot13_encryption): add encrypt method

---
+++ src/encoders/rot13_encryption.py
@@ -0,0 +1,16 @@
+from encryption import Encryption
+
+
+class Rot13Encryption(Encryption):
+    def encrypt(self, text: str) -> str:
+        encrypted_text = ""
+        for char in text:
+            if 'A' <= char <= 'Z':
+                encrypted_text += chr(((ord(char) - ord('A') + 13) % 26) + ord('A'))
+            elif 'a' <= char <= 'z':
+                encrypted_text += chr(((ord(char) - ord('a') + 13) % 26) + ord('a'))
+            else:
+                encrypted_text += char
+        return encrypted_text
+
+
