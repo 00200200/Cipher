97084605638922640bd18562bc8e59eaca427735
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 19:05:52 +0100

    docs(test_buffer): add docstrings to test_buffer  methods

--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -2,14 +2,24 @@ from src.data.buffer import Buffer
 from src.data.text import Text
 
 
-def test_buffer_add():
+def test_buffer_add() -> None:
+    """
+    Tests add() function of Buffer Class.
+    Checking if Text object is added to buffer.
+    :return: None
+    """
     buffer = Buffer()
     text = Text("test", "rot13", "encrypted")
     buffer.add(text)
     assert buffer.get_all() == [text]
 
 
-def test_buffer_clear():
+def test_buffer_clear() -> None:
+    """
+    Tests clear() function of Buffer class.
+    Checking if buffor is correctly cleared.
+    :return: None
+    """
     buffer = Buffer()
     text = Text("test", "rot13", "encrypted")
     buffer.add(text)
