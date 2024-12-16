59d0a7c43f1c1bff825a3798ba7f7ab959ad7c91
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 14:30:24 +0100

    docs(rot47_encryption): add docstrings to rot47encryption class

--- src/encoders/rot47_encryption.py
+++ src/encoders/rot47_encryption.py
@@ -2,8 +2,15 @@ from encryption import Encryption
 
 
 class Rot47Encryption(Encryption):
+    """
+    Implements rot47 encryption and decryption
+    """
     def encrypt(self, text: str) -> str:
-
+        """
+        Encrypt the given text using ROT47.
+        :param text: Text to encrypt
+        :return: Encrypted Text
+        """
         encrypted_text = ""
         for char in text:
             if 33 <= ord(char) <= 126:
@@ -13,4 +20,10 @@ class Rot47Encryption(Encryption):
         return encrypted_text
 
     def decrypt(self, text: str) -> str:
+        """
+        Decrypt the given Text.
+        :param text: text to dectypt.
+        :return: decrypted text.
+        """
         return self.encrypt(text)
+    
