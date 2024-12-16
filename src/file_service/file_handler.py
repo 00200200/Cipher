2fbe5bd03c4e171ba05774339afa1a04f9b5749b
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 13:42:46 +0100

    docs(filehandles): add docstrings for load method.

--- src/file_service/file_handler.py
+++ src/file_service/file_handler.py
@@ -4,9 +4,16 @@ from typing import List
 
 
 class FileHandler:
-
+    """
+    Handles file operations saving/loading Text Objects.
+    """
     @staticmethod
     def load(filename: str) -> List[Text]:
+        """
+        Load a list of Text objects from JSON file.
+        :param filename: path to JSON file.
+        :return: A list of Text objects
+        """
         with open(filename, "r", encoding='utf-8') as file:
             data = json.load(file)
             return [Text(item["text"], item["rot_type"], item["status"]) for item in data]
