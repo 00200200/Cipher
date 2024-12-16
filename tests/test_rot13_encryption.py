0ba64bb8e238b7fd5001b3e7afac86d1daaa8a57
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 19:03:24 +0100

    test(rot13_encryption): add unit test for Rot13Encryption class.

---
+++ tests/test_rot13_encryption.py
@@ -0,0 +1,20 @@
+from src.encoders.rot13_encryption import Rot13Encryption
+import pytest
+
+
+@pytest.mark.parametrize("input_text, output", [
+    ("hello", "uryyb"), ("HELLO", "URYYB"), ("12345", "12345"), ("''", "''"),
+    ("test example", "grfg rknzcyr"), ("TEST EXAMPLE", "GRFG RKNZCYR"), ("Test Example", "Grfg Rknzcyr"),
+])
+def test_rot13_encrypt(input_text, output):
+    rot13 = Rot13Encryption()
+    assert rot13.encrypt(input_text) == output
+
+
+@pytest.mark.parametrize("input_text, output", [
+    ("uryyb", "hello"), ("URYYB", "HELLO"), ("12345", "12345"), ("''", "''"),
+    ("grfg rknzcyr", "test example"), ("GRFG RKNZCYR", "TEST EXAMPLE"), ("Grfg Rknzcyr", "Test Example"),
+])
+def test_rot13_decrypt(input_text, output):
+    rot13 = Rot13Encryption()
+    assert rot13.decrypt(input_text) == output
