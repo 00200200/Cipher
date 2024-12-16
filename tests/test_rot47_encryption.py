c14ef2c2545438b1fd5d7fa7ef83c866a2e0e2a3
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 19:03:36 +0100

    test(rot47_encryption): add unit test for Rot47Encryption class.

---
+++ tests/test_rot47_encryption.py
@@ -0,0 +1,28 @@
+from src.encoders.rot47_encryption import Rot47Encryption
+import pytest
+
+@pytest.mark.parametrize("input_text, output", [
+    ("hello", "96==@"),
+    ("HELLO", "wt{{~"),
+    ("12345", "`abcd"),
+    ("", ""),
+    ("test example", "E6DE 6I2>A=6"),
+    ("TEST EXAMPLE", "%t$% t)p|!{t"),
+    ("Test Example", "%6DE tI2>A=6"),
+])
+def test_rot47_encrypt(input_text,output):
+    rot47 = Rot47Encryption()
+    assert rot47.encrypt(input_text) == output
+
+@pytest.mark.parametrize("input_text, output", [
+    ("96==@", "hello"),
+    ("wt{{~", "HELLO"),
+    ("`abcd", "12345"),
+    ("", ""),
+    ("E6DE 6I2>A=6", "test example"),
+    ("%t$% t)p|!{t", "TEST EXAMPLE"),
+    ("%6DE tI2>A=6", "Test Example"),
+])
+def test_rot47_decrypt(input_text, output):
+    rot47 = Rot47Encryption()
+    assert rot47.decrypt(input_text) == output
