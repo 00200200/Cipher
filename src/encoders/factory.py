e608869cfd7fef67af49c0326e17b12ffbfff92e
Michał Furgała <83299832+00200200@users.noreply.github.com>
2024-12-16 14:01:38 +0100

    docs(factory): add docstrings to EncryptionFactory class.

--- src/encoders/factory.py
+++ src/encoders/factory.py
@@ -4,8 +4,16 @@ from encryption import Encryption
 
 
 class EncryptionFactory:
+    """
+    Factory class to create encryption objects.
+    """
     @staticmethod
     def get_encryption(rot_type: str) -> Encryption:
+        """
+        Returns the encryption class based on rot_type
+        :param rot_type: Type of encryption ('rot13', 'rot47')
+        :return:  Instance of and encryption class.
+        """
         if rot_type == "rot13":
             return Rot13Encryption()
         elif rot_type == "rot47":
